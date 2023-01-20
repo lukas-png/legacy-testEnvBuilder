47f1014dbcea197300db82e572112d79fedcdf50
Lukas Dönges <lukasdoenges@t-online.de>
2023-01-20 23:54:02 +0100

    remove junk and better default variablen

--- env.ts
+++ env.ts
@@ -1,8 +1,8 @@
 import { env } from './interface/envinterface';
 
 export const envVariables: env = {
-    sparkyurl: 'http://localhost:8080',
-    backendurl: 'http://localhost:3000',
+    sparkyurl: 'http://sparky-service:8080',
+    backendurl: 'http://stu-mgmt-backend:3000',
     submissionurl: 'https://sparkydev.pearsoncmg.com',
     courseid: '123456'
 };
