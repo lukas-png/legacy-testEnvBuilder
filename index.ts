a6f9f3ae5a1b6e4f3e399ed0b7178e0223a3c66b
Lukas Dönges <lukasdoenges@t-online.de>
2023-01-20 20:13:20 +0100

    added waiting time

--- index.ts
+++ index.ts
@@ -6,31 +6,33 @@ import SparkyAuthentifikation from "./services/SparkyAuthentifikation";
 import Stumgmtbackend from "./services/Stumgmtbackend";
 import Docker from "./docker";
 
+wait(20000).then(() => {
+    console.log("Starting test environment creation");
+    let api = new SparkyAuthentifikation();
+    api.authenticate("admin_user", "admin_pw").then((response) => {
+        if(response.status) {
+            if(response.data != null) {
+            let token = response.data.token;
+            if(token != null && token.token != null)    {
+                let accesstoken = token.token;
 
-let api = new SparkyAuthentifikation();
-api.authenticate("admin_user", "admin_pw").then((response) => {
-    if(response.status) {
-        if(response.data != null) {
-          let token = response.data.token;
-          if(token != null && token.token != null)    {
-            let accesstoken = token.token;
-
-            Docker.createTestEnvironment(api, new Stumgmtbackend(accesstoken)).then(() => {
-                console.log("Test environment created");
-            } ).catch((e) => {
-                console.log(e);
-            });
-
-
-          }
-
-        }
-
-        }
-    
-   
+                Docker.createTestEnvironment(api, new Stumgmtbackend(accesstoken)).then(() => {
+                    console.log("Test environment created");
+                } ).catch((e) => {
+                    console.log(e);
+                });
 
 
+            }
+            }
+            }
+        
+        });
     });
+
+    function wait(seconds: number) {
+        return new Promise(vars => setTimeout(vars, seconds));
+        
+    }
 
 
