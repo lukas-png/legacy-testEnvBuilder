34a921f61783bb29cfb1d54cc4c06c463ed725f5
Lukas Dönges <lukasdoenges@t-online.de>
2023-01-21 15:45:16 +0100

    remove junk

--- env.ts
+++ env.ts
@@ -3,7 +3,7 @@ import { env } from './interface/envinterface';
 export const envVariables: env = {
     sparkyurl: 'http://sparky-service:8080',
     backendurl: 'http://stu-mgmt-backend:3000',
-    submissionurl: 'https://sparkydev.pearsoncmg.com',
+    submissionurl: '',
     courseid: '123456'
 };
     // sparkyurl: 'http://sparky-service:8080',
--- interface/ErrorInterface.tsx
+++
@@ -1,6 +0,0 @@
-export interface ErrorInterface<T> {
-    message?: string;
-    code: number;
-    status: boolean;
-    data: T | null;
-}
